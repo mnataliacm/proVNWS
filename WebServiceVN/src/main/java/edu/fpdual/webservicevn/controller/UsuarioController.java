a15712c22415c5a6c0b66f8bb3df34f5ab1d3f36
Natalia Castillo Muñoz <mnacamu@gmail.com>
2022-05-24 12:26:08 +0200

    subo

--- WebServiceVN/src/main/java/edu/fpdual/webservicevn/controller/UsuarioController.java
+++ WebServiceVN/src/main/java/edu/fpdual/webservicevn/controller/UsuarioController.java
@@ -1,9 +1,8 @@
 package edu.fpdual.webservicevn.controller;
 
-import edu.fpdual.proyectovn.model.manager.UsuarioManager;
+import edu.fpdual.proyectovn.model.dao.Usuario;
 import edu.fpdual.proyectovn.model.manager.implement.UsuarioManagerImpl;
 import edu.fpdual.webservicevn.service.UsuarioService;
-
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
@@ -14,15 +13,16 @@ import java.sql.SQLException;
 
 @Path("/usuario")
 public class UsuarioController {
-
+  Usuario usuario = new Usuario();
   private final UsuarioService usuarioService;
 
   public UsuarioController (){
-    this.usuarioService = new UsuarioService(new ???);
+    this.usuarioService = new UsuarioService(new UsuarioManagerImpl());
   }
 
 
   @GET
+  @Path("/listado")
   @Produces(MediaType.APPLICATION_JSON)
   public Response todosUsuarios() throws SQLException, ClassNotFoundException {
     return Response.ok().entity(usuarioService.todosUsuarios()).build();
--- WebServiceVN/src/main/java/edu/fpdual/webservicevn/service/UsuarioService.java
+++ WebServiceVN/src/main/java/edu/fpdual/webservicevn/service/UsuarioService.java
@@ -2,22 +2,23 @@ package edu.fpdual.webservicevn.service;
 
 import edu.fpdual.proyectovn.model.connector.Connector;
 import edu.fpdual.proyectovn.model.dao.Usuario;
-import edu.fpdual.webservicevn.controller.UsuarioController;
+import edu.fpdual.proyectovn.model.manager.implement.UsuarioManagerImpl;
 
 import java.sql.Connection;
 import java.sql.SQLException;
-import java.util.List;
+import java.util.Set;
 
 public class UsuarioService {
 
-  private UsuarioController usuarioController;
-  public UsuarioService(UsuarioController usuarioController) {
-    this.usuarioController = usuarioController;
+  private final UsuarioManagerImpl usuarioManager;
+
+  public UsuarioService(UsuarioManagerImpl usuarioManager) {
+    this.usuarioManager = usuarioManager;
   }
 
-public List<Usuario> todosUsuarios() throws SQLException, ClassNotFoundException {
+public Set<Usuario> todosUsuarios() throws SQLException, ClassNotFoundException {
     try (Connection con = new Connector().getConnection()) {
-      return (List<Usuario>) usuarioController.todosUsuarios();
+      return usuarioManager.todosUsuarios(con);
     }
 }
 
