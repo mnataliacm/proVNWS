78260a3e62e8be5a32e11044bb319e3a5de06ebd
Veronica Gonzalez Pons <veronicagnzpns@gmail.com>
2022-06-13 07:36:03 +0200

    commit bien

--- WebServiceVN/src/test/java/edu/fpdual/webservicevn/model/dao/EmpresaTest.java
+++ WebServiceVN/src/test/java/edu/fpdual/webservicevn/model/dao/EmpresaTest.java
@@ -1,2 +1,46 @@
-package edu.fpdual.webservicevn.model.dao;public class EmpresaTest {
+package edu.fpdual.webservicevn.model.dao;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.stubbing.Answer;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.when;
+
+
+@ExtendWith(MockitoExtension.class)
+class EmpresaTest {
+
+    @Mock
+    private ResultSet resultSet;
+
+    @Test
+    void constructor() throws SQLException {
+        Empresa empresa1 = new Empresa(25, "Gonzalez S.A");
+
+        when(resultSet.getInt(any())).thenAnswer((Answer<Integer>) invocationOnMock -> {
+            if (invocationOnMock.getArgument(0).equals("IDemp")) {
+                return empresa1.getId();
+            } else {
+                return null;
+            }
+        });
+
+        when(resultSet.getString(any())).thenAnswer((Answer<String>) invocationOnMock -> {
+            if (invocationOnMock.getArgument(0).equals("NomEmp")) {
+                return empresa1.getNomEmp();
+            } else {
+                return null;
+            }
+        });
+
+        Empresa empresa2 = new Empresa(resultSet);
+        assertEquals(empresa1, empresa2);
+    }
 }
--- WebServiceVN/src/test/java/edu/fpdual/webservicevn/model/dao/ReservaTest.java
+++ WebServiceVN/src/test/java/edu/fpdual/webservicevn/model/dao/ReservaTest.java
@@ -1,4 +1,59 @@
 package edu.fpdual.webservicevn.model.dao;
 
-public class ReservaTest {
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.stubbing.Answer;
+
+import java.sql.Date;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Time;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class ReservaTest {
+
+    @Mock
+    private ResultSet resultSet;
+
+    @Test
+    void constructor() throws SQLException {
+        Reservas reservas1 = new Reservas(55, 5, 3, 10-12-2022, 25);
+
+        when(resultSet.getInt(any())).thenAnswer((Answer<Integer>) invocationOnMock -> {
+            if (invocationOnMock.getArgument(0).equals("IDres")) {
+                return reservas1.getIdRes();
+            } else if (invocationOnMock.getArgument(0).equals("IDusu")) {
+                return reservas1.getIdUsu();
+            } else if (invocationOnMock.getArgument(0).equals("IDact")) {
+                return reservas1.getIdAct();
+            } else {
+                return null;
+            }
+        });
+
+        when(resultSet.getDate(any())).thenAnswer((Answer<Date>) invocationOnMock -> {
+            if (invocationOnMock.getArgument(0).equals("Fecha")) {
+                return reservas1.getFecha();
+            } else {
+                return null;
+            }
+        });
+
+        when(resultSet.getTime(any())).thenAnswer((Answer<Time>) invocationOnMock -> {
+            if (invocationOnMock.getArgument(0).equals("Hora")) {
+                return reservas1.getHora();
+            } else {
+                return null;
+            }
+        });
+
+        Reservas reservas2 = new Reservas(resultSet);
+        assertEquals(reservas1, reservas2);
+    }
 }
